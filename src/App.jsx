7eebd9445dd51501b6033ad759faf07173cef959
Vinayaka Iyer <vinayakaiyer999@gmail.com>
2025-01-28 11:42:21 +0530

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import SolutionsPage from "@/pages/SolutionsPage";
 import PricingPage from "@/pages/PricingPage";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 
 function App() {
   return (
     <>
       <Provider store={store}>
         <Router>
+        <Navbar />
           <Routes>
               {/* protected routes examples */}
               {/* <Route element={<RequireAuth />}>
--- src/services/auth/authApiSlice.js
+++ src/services/auth/authApiSlice.js
@@ -1,4 +1,4 @@
-import { apiSlice } from "./apiSlice";
+import { apiSlice } from "../apiSlice";
 
 export const authApiSlice = apiSlice.injectEndpoints({
      endpoints: builder =>({
